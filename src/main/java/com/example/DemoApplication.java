50c2762fd04213193dd7728c8a3e2d492f30d915
laileon <leon@igola.com>
2017-05-01 15:37:16 +0800

    2nd commit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,7 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableGlobalMethodSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringBootApplication
 public class DemoApplication {
 
